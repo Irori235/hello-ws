909c2630b04297e02b97f7c9cbc54df5665b3731
Irori235 <Irori235@gmail.com>
2024-02-07 15:12:02 +0900

    🌱 init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ws-hello
+
+go 1.21.1
+
+require golang.org/x/net v0.20.0
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"golang.org/x/net/websocket"
+)
+
+type server struct {
+	clients map[*websocket.Conn]bool
+	mu      sync.Mutex
+}
+
+func NewServer() *server {
+	return &server{
+		clients: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *server) sendToEveryone(msg string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for conn := range s.clients {
+		err := websocket.Message.Send(conn, msg)
+		if err != nil {
+			log.Println("Broadcast Error:", err)
+			conn.Close()
+			delete(s.clients, conn)
+		}
+	}
+}
+
+func (s *server) webSocketHandler(ws *websocket.Conn) {
+	defer ws.Close()
+
+	s.mu.Lock()
+	s.clients[ws] = true
+	s.mu.Unlock()
+
+	err := websocket.Message.Send(ws, "Server: Hello, Client!")
+	if err != nil {
+		log.Println(err)
+	}
+
+	for {
+		msg := ""
+		err := websocket.Message.Receive(ws, &msg)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		broadcastMsg := fmt.Sprintf("Server: '%s' received.", msg)
+		s.sendToEveryone(broadcastMsg)
+	}
+}
+
+func main() {
+	srv := NewServer()
+
+	http.Handle("/", http.FileServer(http.Dir("./tmp")))
+	http.Handle("/ws", websocket.Handler(srv.webSocketHandler))
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
